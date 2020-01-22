7791e323f158306e099e295eba625f0b91fc50d5
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2020-01-22 13:24:15 +0530

    Port added and changes made

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors());
 
 /*routes*/
 
-app.get('/',(req,res)=>{  res.send("It is working");})
+app.get('/',(req,res)=>{  res.send("It is working.");})
 
 //signin
 app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)});
@@ -46,6 +46,6 @@ app.put('/image',(req,res)=>{image.imageCount(req,res,db)});
 //API call
 app.post('/imageurl',(req,res)=>{image.handleAPIcall(req,res)});
 
-app.listen(process.env.PORT ||3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log(`app is running on port ${process.env.PORT}`)
 }); 
